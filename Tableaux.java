723c555b35a1ff10da556c7d7e0e91ade181cd04
Vincent12345 <vinct27@hotmail.ca>
2020-01-24 11:46:26 -0500

    Max Min Moy et fouille ajouté

--- Tableaux.java
+++ Tableaux.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
  * @author France Beaudoin
  */
 public class Tableaux {
-    private static void permute(int[] tab, int i1, int i2){
+    private static void permute(int[] tab, int i1, int i2) {
         int transit = tab[i1];
         tab[i1] = tab[i2];
         tab[i2] = transit;
     }
 
-    private static void permute(String[] tab, int i1, int i2){
+    private static void permute(String[] tab, int i1, int i2) {
         String transit = tab[i1];
         tab[i1] = tab[i2];
         tab[i2] = transit;
     }
 
-    public static int[] trierCroissantSSS(int[] tab){
+    public static int[] trierCroissantSSS(int[] tab) {
         int imin;
-        for (int i=0; i<tab.length-1; i++){
+        for (int i = 0; i < tab.length - 1; i++) {
             imin = i;
-            for (int j=i+1; j<tab.length; j++){
+            for (int j = i + 1; j < tab.length; j++) {
                 if (tab[j] < tab[imin])
                     imin = j;
             }
@@ -30,11 +30,25 @@ public class Tableaux {
         return tab;
     }
 
-    public static void trierCroissantSSS(String[] tab){
+    public static int[] trierDecroissantSSS(int[] tab) {
         int imin;
-        for (int i=0; i<tab.length-1; i++){
+        for (int i = 0; i < tab.length - 1; i++) {
             imin = i;
-            for (int j=i+1; j<tab.length; j++){
+            for (int j = i + 1; j < tab.length; j++) {
+                if (tab[j] > tab[imin])
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+        return tab;
+    }
+
+    public static void trierDecroissantSSS(String[] tab) {
+        int imin;
+        for (int i = 0; i < tab.length - 1; i++) {
+            imin = i;
+            for (int j = i + 1; j < tab.length; j++) {
                 if (Integer.parseInt(tab[j]) < Integer.parseInt(tab[imin]))
                     imin = j;
             }
@@ -43,7 +57,60 @@ public class Tableaux {
         }
     }
 
-    public static void print(int[] tab){
-        System.out.print(Arrays.toString(tab));
+    public static void print(int[] tab) {
+        System.out.println(Arrays.toString(tab));
     }
+
+    public static int maximum(int[] tab) {
+        int imin;
+        for (int i = 0; i < tab.length - 1; i++) {
+            imin = i;
+            for (int j = i + 1; j < tab.length; j++) {
+                if (tab[j] < tab[imin])
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+        return tab[tab.length - 1];
+
+    }
+
+    public static int minimum(int[] tab) {
+        int imin;
+        for (int i = 0; i < tab.length - 1; i++) {
+            imin = i;
+            for (int j = i + 1; j < tab.length; j++) {
+                if (tab[j] < tab[imin])
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+        return tab[0];
+
+    }
+
+    public static double moyenne(int[] tab){
+        int moyenne = 0;
+        for (int i=0;i<tab.length-1;i++ ){
+            moyenne += tab[i];
+        }
+        return moyenne/tab.length;
+    }
+
+    public static int fouilleSeq(int[] tab, int nombre){
+        int i = 0;
+        boolean trouve = false;
+        while (i<tab.length && !trouve){
+            if (tab[i] == nombre)
+                trouve = true;
+            else i++;
+
+        }
+        if (trouve)
+            return tab[i];
+        else return -1;
+    }
+
 }
